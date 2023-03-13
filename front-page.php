ed3518fdd17982ec0ed404467b7858cc2d8be6d4
HaruTechCode <harutechcode@gmail.com>
2023-03-13 21:28:46 +0900

    サービスの途中まで

--- front-page.php
+++ front-page.php
@@ -2,7 +2,7 @@
 <main>
     <div class="fv">
       <div class="fv__inner">
-        <div class="fv__img js-slider">
+        <div class="fv__slick js-slick">
           <img src="<?php echo esc_url(get_theme_file_uri("/images/fv01.jpg")) ?>" alt="">
           <img src="<?php echo esc_url(get_theme_file_uri("/images/fv02.jpg")) ?>" alt="">
           <img src="<?php echo esc_url(get_theme_file_uri("/images/fv03.jpg")) ?>" alt="">
@@ -15,5 +15,65 @@
           </div>
       </div>
     </div>
+  </div>
+  <div class="flowing-box js-flowing-box">
+    <div class="flowing-box__inner js-flowing-box__inner">
+      <div class="flowing-box__body js-flowing-box__body">
+        <p>Good life for Good Car</p>
+      </div>
+    </div>
+  </div>
+  <section class="top-about">
+    <div class="top-about__inner inner">
+      <img src="<?php echo esc_url(get_theme_file_uri("/images/top-about-img.jpg")) ?>" alt="" class="top-about__img">
+      <div class="top-about__body">
+        <h3 class="top-about__title">About us</h3>
+        <div class="top-about__text">
+          弊社は、輸入車選びのすべてを見つめ直し、新たなスタイルを提案するインポート・セレクト・ブランドです。<br>
+          これまでの憧れだけや批評家の意見といったイメージで選ぶのではなく、オーナー様となる方がクルマから直接感じるリアルな感性を第一にした、最良の一台との出会いをコーディネートいたします。
+        </div>
+        <div class="top-about__link-wrapper">
+          <a class="top-about__link animation-link">read more</a>
+        </div>
+      </div>
+    </div>
+  </section>
+  <section class="top-service">
+    <div class="top-service__inner inner">
+      <h2 class="top-service-heading large-heading large-heading--right">Service</h2>
+      <ul class="top-service__cards">
+        <li class="top-service__card card">
+          <img src="<?php echo esc_url(get_theme_file_uri("/images/top_service1.jpg")) ?>" alt="" class="card__img">
+          <div class="card__img"></div>
+          <div class="card__title-wrapper">
+            <div class="card__title-sub">Service01</div>
+            <div class="card__title-main">購入サポート</div>
+          </div>
+          <div class="card__text">国を超え、メーカーを超え、高級の本質をお届けする特別な一台との出会いをサポートします。</div>
+          <a class="animation-link">read more</a>
+        </li>
+        <li class="top-service__card card">
+          <img src="<?php echo esc_url(get_theme_file_uri("/images/top_service2.jpg")) ?>" alt="" class="card__img">
+          <div class="card__img"></div>
+          <div class="card__title-wrapper">
+            <div class="card__title-sub">Service02</div>
+            <div class="card__title-main">修理・整備</div>
+          </div>
+          <div class="card__text">高度な輸入車修理技術と数多くの修理実績、熟練の技術・設備であなたの愛車を完全に直します。</div>
+          <a class="animation-link">read more</a>
+        </li>
+        <li class="top-service__card card">
+          <img src="<?php echo esc_url(get_theme_file_uri("/images/top_service3.jpg")) ?>" alt="" class="card__img">
+          <div class="card__img"></div>
+          <div class="card__title-wrapper">
+            <div class="card__title-sub">Service03</div>
+            <div class="card__title-main">車検・点検</div>
+          </div>
+          <div class="card__text">輸入車の取り扱いが県内トップクラス。専門の整備工場へ任せたいなら弊社へご相談ください。</div>
+          <a class="animation-link">read more</a>
+        </li>
+      </ul>
+    </div>
+  </section>
 </main>
 <?php get_footer(); ?>
--- js/script.js
+++ js/script.js
@@ -1,6 +1,47 @@
 $(function () {
-    $(".js-slider").slick({
+    $(".js-slick").slick({
+        //開発中はautoplayしない
         //autoplay: true,
-        autoplaySpeed: 5000,
+        autoplaySpeed: 3000,
     });
+
+    //流れるテキスト
+    //変数
+    var $flowText = $(".js-flowing-box"),
+        $fixBox = $flowText.find(".js-flowing-box__inner"),
+        $flowBox = $fixBox.find(".js-flowing-box__body"),
+        flowbox_width = $flowBox.width(), //流れてくるBoxの横幅
+        flowTime = 10000, //全てのテキストが流れ終わるまでの時間．初期値10000ms
+        easing = "linear", //アニメーションの軌跡
+        right_start, //流れるBoxの右側の初期位置を入れる変数
+        right_running, //動作中の右側の位置を入れる変数
+        timer;
+
+    //流れてくるBoxの初期値
+    $flowBox.css({ left: "100%" });
+    //流れるBoxの右側の初期位置
+    right_start = $flowBox.offset().left + flowbox_width;
+
+    //ボックスを流す関数
+    function flowingStart() {
+        $flowBox.animate(
+            { left: -flowbox_width },
+            flowTime,
+            easing,
+            function () {
+                //アニメーション後に行うコールバック関数
+                $flowBox.css({ left: "100%" });
+            }
+        );
+        flowTime = 10000; //次のアニメーションの時間を設定
+    }
+
+    //繰り返し，flowingStart()を実行するための関数
+    function flowingMonitor() {
+        timer = setInterval(function () {
+            flowingStart();
+        }, 300);
+    }
+
+    flowingMonitor();
 });
