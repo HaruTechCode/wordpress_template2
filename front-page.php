a57faca0cd091ee2210000aeb83a17d6f9634321
HaruTechCode <harutechcode@gmail.com>
2023-03-13 23:49:48 +0900

    worksの途中まで

--- front-page.php
+++ front-page.php
@@ -50,7 +50,9 @@
             <div class="card__title-main">購入サポート</div>
           </div>
           <div class="card__text">国を超え、メーカーを超え、高級の本質をお届けする特別な一台との出会いをサポートします。</div>
-          <a class="animation-link">read more</a>
+          <div class="card__link-wrapper">
+            <a class="card__link animation-link animation-link--button">read more</a>
+          </div>
         </li>
         <li class="top-service__card card">
           <img src="<?php echo esc_url(get_theme_file_uri("/images/top_service2.jpg")) ?>" alt="" class="card__img">
@@ -60,7 +62,9 @@
             <div class="card__title-main">修理・整備</div>
           </div>
           <div class="card__text">高度な輸入車修理技術と数多くの修理実績、熟練の技術・設備であなたの愛車を完全に直します。</div>
-          <a class="animation-link">read more</a>
+          <div class="card__link-wrapper">
+            <a class="card__link animation-link animation-link--button">read more</a>
+          </div>
         </li>
         <li class="top-service__card card">
           <img src="<?php echo esc_url(get_theme_file_uri("/images/top_service3.jpg")) ?>" alt="" class="card__img">
@@ -70,7 +74,28 @@
             <div class="card__title-main">車検・点検</div>
           </div>
           <div class="card__text">輸入車の取り扱いが県内トップクラス。専門の整備工場へ任せたいなら弊社へご相談ください。</div>
-          <a class="animation-link">read more</a>
+          <div class="card__link-wrapper">
+            <a class="card__link animation-link animation-link--button">read more</a>
+          </div>
+        </li>
+      </ul>
+    </div>
+  </section>
+  <!-- TODO 画像名統一。ワードプレスから引っ張ってくるならしなくていいから後回しでいいけど。 -->
+  <section class="top-works">
+    <div class="top-works__inner inner">
+      <!-- TODO inview使って左からフェードイン -->
+      <h2 class="top-works-heading large-heading">Works</h2>
+      <!-- TODO パーシャル使ったり、他との共通点を探したり、ワードプレスのデータを入れたり -->
+      <ul class="top-works__articles">
+        <li class="top-works__article">
+          <img src="<?php echo esc_url(get_theme_file_uri("/images/top_works1.jpg")) ?>" alt="" class="top-works__article-img">
+          <div class="top-works__article-body">
+            <div class="top-works__article-category">修理・整備</div>
+            <div class="top-works__article-title">車のドアが開けられなくなった｜ドアロックの交換と...</div>
+            <div class="top-works__article-text">弊社は、輸入車選びのすべてを見つめ直し、新たなスタイルを提案するインポート・セレクト・ブランドです。これまでの憧れだけや批評家の意見といったイメージで選ぶのではなく...</div>
+            <time class="top-works__article-date" datetime="2022-09-02">2022.09.02</time>
+          </div>
         </li>
       </ul>
     </div>
