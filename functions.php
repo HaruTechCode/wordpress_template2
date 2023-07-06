9da52b06ef560ec6fce3a8331ffbc6ca324f8838
HaruTechCode <harutechcode@gmail.com>
2023-07-06 18:50:14 +0900

    tabをスペースに置換

--- functions.php
+++ functions.php
@@ -46,32 +46,32 @@ function my_wp_head()
 add_action("wp_head", "my_wp_head");
 
 function custom_search($search, $wp_query) {
-	global $wpdb;
+    global $wpdb;
 
-	if (!$wp_query->is_search)
-			return $search;
-	if (!isset($wp_query->query_vars))
-			return $search;
+    if (!$wp_query->is_search)
+        return $search;
+    if (!isset($wp_query->query_vars))
+        return $search;
 
-	$search_words = explode(' ', isset($wp_query->query_vars['s']) ? $wp_query->query_vars['s'] : '');
-	if ( count($search_words) > 0 ) {
-			$search = '';
-			$search .= "AND post_type = 'works'";
-			foreach ( $search_words as $word ) {
-					if ( !empty($word) ) {
-							$search_word = '%' . esc_sql( $word ) . '%';
-							$search .= " AND (
-								 {$wpdb->posts}.post_title LIKE '{$search_word}'
-								OR {$wpdb->posts}.post_content LIKE '{$search_word}'
-								OR {$wpdb->posts}.ID IN (
-								SELECT distinct post_id
-								FROM {$wpdb->postmeta}
-								WHERE meta_value LIKE '{$search_word}'
-								)
-							) ";
-					}
-			}
-	}
-	return $search;
+    $search_words = explode(' ', isset($wp_query->query_vars['s']) ? $wp_query->query_vars['s'] : '');
+    if ( count($search_words) > 0 ) {
+        $search = '';
+        $search .= "AND post_type = 'works'";
+        foreach ( $search_words as $word ) {
+            if ( !empty($word) ) {
+                $search_word = '%' . esc_sql( $word ) . '%';
+                $search .= " AND (
+                     {$wpdb->posts}.post_title LIKE '{$search_word}'
+                    OR {$wpdb->posts}.post_content LIKE '{$search_word}'
+                    OR {$wpdb->posts}.ID IN (
+                    SELECT distinct post_id
+                    FROM {$wpdb->postmeta}
+                    WHERE meta_value LIKE '{$search_word}'
+                    )
+                ) ";
+            }
+        }
+    }
+    return $search;
 }
 add_filter('posts_search','custom_search', 10, 2);
