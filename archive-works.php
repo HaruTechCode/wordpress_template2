09f5ec4a299e52a3f56515d28e2ad9bb4b2c5dbf
HaruTechCode <harutechcode@gmail.com>
2023-08-11 21:39:43 +0900

    worksの一旦の完成

--- archive-works.php
+++ archive-works.php
@@ -1,65 +1,79 @@
 <?php get_header(); ?>
-<section class="page-fv">
-    <div class="page-fv__inner">
-        <h2 class="page-fv__heading">
-            <span class="page-fv__heading-main">Works</span>
-            <span class="page-fv__heading-sub">実績紹介</span>
-        </h2>
-        <img src="<?php echo esc_url(get_theme_file_uri("/images/works_fv.jpg")) ?>" alt="" class="page-fv__img">
-    </div>
-</section>
-<div class="flex-wrapper">
-    <aside>
-        <ul class="works-category__list">
-            <li>全ての実績</li>
-            <?php
-            $terms = get_terms('works_category');
+<main>
+    <section class="page-fv">
+        <div class="page-fv__inner">
+            <h2 class="page-fv__heading">
+                <span class="page-fv__heading-main">Works</span>
+                <span class="page-fv__heading-sub">実績紹介</span>
+            </h2>
+            <img src="<?php echo esc_url(get_theme_file_uri("/images/works_fv.jpg")) ?>" alt="" class="page-fv__img">
+        </div>
+    </section>
+    <?php get_template_part("breadcrumb");?>
+    <div class="works__inner inner">
+        <div class="works-category__list-container">
+            <ul class="works-category__list">
+                <li class="category-item-current">すべての実績</li>
+                <?php
+                $terms = get_terms('works_category');
 
-            foreach ( $terms as $term ) {
-                $term_link = get_term_link( $term );
-                echo '<a href="' . esc_url( $term_link ) . '">' . $term->name . '</a>';
-            }
-            ?>
-        </ul>
-    </aside>
-    <main>
-        <section class = "archive-works">
-            <?php if (have_posts()) : ?>
-                <ul>
-                    <?php while (have_posts()) : the_post(); ?>
-                        <li>
-                            <?php if (has_post_thumbnail()): ?>
-                                <?php the_post_thumbnail( 'thumbnail', ['class' => 'archive-works__img']); ?>
-                            <?php endif; ?>
-                            <?php
-                            $terms = get_the_terms($post->ID, 'works_category');
-                            foreach ($terms as $term) {
-                                echo '<a href="' . get_term_link($term) . '">' . $term->name . '</a>';
-                            }
-                            ?>
-                            <a href="<?php the_permalink(); ?>">
-                                <div class="ttl"><?php the_title(); ?></div>
-                            </a>
-                            <div class="date"><?php echo get_the_date('Y.m.d') ?></div>
-                        </li>
-                    <?php endwhile; ?>
-                </ul>
+                foreach ( $terms as $term ) {
+                    $term_link = get_term_link( $term );
+                    echo '<li><a class="category-item-other" href="' . esc_url( $term_link ) . '">' . $term->name . '</a></li>';
+                }
+                ?>
+            </ul>
+        </div>
+
+        <div class="works__content-inner">
+            <?php if(have_posts()): ?>
+                <div class="works__articles-wrapper">
+                    <ul class="works__articles">
+                        <?php while (have_posts()) : the_post(); ?>
+                            <li class="works__article">
+                                <?php if (has_post_thumbnail()): ?>
+                                    <?php the_post_thumbnail( 'medium', ['class' => 'works__article-img']); ?>
+                                <?php else: ?>
+                                    <img src="<?php echo esc_url(get_theme_file_uri("/images/noimage.png")) ?>" alt="" class="works__article-img">
+                                <?php endif; ?>
+                                <div class="works__article-body">
+                                    <?php
+                                    $terms = get_the_terms($post->ID, 'works_category');
+                                    if($terms) {
+                                    foreach ($terms as $term) {
+                                        echo '<div class="works__article-category tag"><a href="' . get_term_link($term) . '">' . $term->name . '</a></div>';
+                                    }
+                                    }
+                                    ?>
+                                    <div class="works__article-title">
+                                        <a href="<?php the_permalink(); ?>">
+                                            <span class="works__underline"><?php the_title(); ?></span>
+                                        </a>
+                                    </div>
+                                    <div class="works__date date"><?php echo get_the_date('Y.m.d') ?></div>
+                                </div>
+                            </li>
+                        <?php endwhile; ?>
+                    </ul>
+                    <div>
+                        <?php
+                        $args = array(
+                            'mid_size' => 2,
+                            'prev_text' => '前',
+                            'next_text' => '後',
+                            'screen_reader_text' => 'ページャー'
+                        );
+                        $args = array(
+                            'mid_size'   => 1,
+                            'prev_text'  => '<span class="pagination__prev"></span>',
+                            'next_text'  => '<span class="pagination__next"></span>',
+                        );
+                        the_posts_pagination($args);
+                        ?>
+                    </div>
+                </div>
             <?php endif; ?>
-        </section>
-        <?php
-        // $args = array(
-        //     'mid_size' => 2,
-        //     'prev_text' => '前',
-        //     'next_text' => '後',
-        //     'screen_reader_text' => 'ページャー'
-        // );
-          $args = array(
-            'mid_size'   => 1,
-            'prev_text'  => '<span class="pagination__prev"></span>',
-            'next_text'  => '<span class="pagination__next"></span>',
-          );
-        the_posts_pagination($args);
-        ?>
-    </main>
-</div>
+        </div>
+    </div>
+</main>
 <?php get_footer(); ?>
--- archive.php
+++ archive.php
@@ -6,6 +6,6 @@ var_dump($cat_id == 0);
 
 //echo $cat_id;
 //get_template_part("temp/_archive" , get_post_type());
-get_template_part("temp/_archive-". get_post_type());
+get_template_part("partials/_archive-". get_post_type());
 ?>
 <!-- </?php get_footer(); ?> -->
