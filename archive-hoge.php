9eb48443cc3a3310c62741a47874f1242f569304
HaruTechCode <harutechcode@gmail.com>
2023-08-12 14:58:11 +0900

    仮で記述していたcontactフォームの下書きを削除。続き

--- archive-hoge.php
+++ archive-hoge.php
@@ -1,20 +1,3 @@
 <?php get_header(); ?>
 <main>archive-hoge.php</main>
 <?php get_footer(); ?>
-
-<div class="contact">
-    <div class="contact-form">
-        <div class="contact-form__item">
-            <label class="contact-form__label" for="">
-                <div class="contact-form__label-name">
-                    会社名
-                    <span class="contact-form__label-asterisk">*</span>
-                </div>
-                <div class="contact-form__label-description">
-                    個人の場合は「個人」と入力してください
-                </div>
-            </label>
-            form
-        </div>
-    </div>
-</div>
