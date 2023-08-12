13a06f6594f062b4dc8879318f00f50a53f7c0d6
HaruTechCode <harutechcode@gmail.com>
2023-08-12 14:55:01 +0900

    contactページを作成

--- archive-hoge.php
+++ archive-hoge.php
@@ -1,3 +1,70 @@
 <?php get_header(); ?>
 <main>archive-hoge.php</main>
 <?php get_footer(); ?>
+
+<div class="contact">
+    <div class="contact-form">
+        <div class="contact-form__item">
+            <label class="contact-form__label" for="">
+                <div class="contact-form__label-name">
+                    会社名
+                    <span class="contact-form__label-asterisk">*</span>
+                </div>
+                <div class="contact-form__label-description">
+                    個人の場合は「個人」と入力してください
+                </div>
+            </label>
+            form
+        </div>
+    </div>
+</div>
+弊社へのお問い合わせは
+下記フォームより受け付けております。
+担当者が確認後、折り返しご連絡させていただきます。
+
+
+<div class="contact-form">
+    <div class="contact-form__item">
+        <label class="contact-form__label" for="company">
+            <span class="contact-form__label-name">会社名<span class="contact-form__label-asterisk">*</span></span><span class="contact-form__label-description">個人の場合は「個人」と入力してください</span>
+        </label>
+        [text* company placeholder "例）株式会社ABC"]
+    </div>
+    <div class="contact-form__item">
+        <label class="contact-form__label" for="yourname">
+            <span class="contact-form__label-name">ご担当者名<span class="contact-form__label-asterisk">*</span></span><span class="contact-form__label-description">個人の場合は氏名を入力してください</span>
+        </label>
+        [text* yourname placeholder "例）輸入車　太郎"]
+    </div>
+    <div class="contact-form__item">
+        <label class="contact-form__label" for="tel">
+            <span class="contact-form__label-name">電話番号<span class="contact-form__label-asterisk">*</span></span>
+        </label>
+        [text* tel placeholder "例）022-000-0000"]
+    </div>
+    <div class="contact-form__item">
+        <label class="contact-form__label" for="email">
+            <span class="contact-form__label-name">メールアドレス<span class="contact-form__label-asterisk">*</span></span>
+        </label>
+        [text* email placeholder "例）abcdefghijk@example.com"]
+    </div>
+    <div class="contact-form__item">
+        <label class="contact-form__label" for="contact_type">
+            <span class="contact-form__label-name">お問い合わせの種類<span class="contact-form__label-asterisk">*</span></span>
+        </label>
+        []
+    </div>
+    <div class="contact-form__item">
+        <label class="contact-form__label" for="contact_content">
+            <span class="contact-form__label-name">お問い合わせ内容<span class="contact-form__label-asterisk">*</span></span>
+        </label>
+        []
+    </div>
+    <div class="contact-form__item">
+        <span class="contact-form__label-asterisk">*</span>
+        []
+        <label class="contact-form__label" for="agree">
+            <span class="contact-form__label-name">「個人情報の取り扱いについて」同意する</span>
+        </label>
+    </div>
+</div>
--- page-contact.php
+++ page-contact.php
@@ -1,12 +1,24 @@
 <?php get_header(); ?>
-<?php if (have_posts()) : ?>
-	<ul>
-		<?php while (have_posts()) : the_post(); ?>
-            <div class="ttl"><?php the_title(); ?></div>
-            <div class="content"><?php the_content(); ?></div>
-
-		<?php endwhile; ?>
-	</ul>
-<?php endif; ?>
+    <section class="page-fv">
+        <div class="page-fv__inner">
+            <h2 class="page-fv__heading">
+                <span class="page-fv__heading-main">Contact</span>
+                <span class="page-fv__heading-sub">お問い合わせ</span>
+            </h2>
+            <img src="<?php echo esc_url(get_theme_file_uri("/images/contact_fv.jpg")) ?>" alt="" class="page-fv__img">
+        </div>
+    </section>
+    <?php get_template_part("breadcrumb");?>
+	<div class="contact">
+		<div class="contact__inner">
+			<?php if (have_posts()) : ?>
+				<ul>
+					<?php while (have_posts()) : the_post(); ?>
+						<div class="content"><?php the_content(); ?></div>
+					<?php endwhile; ?>
+				</ul>
+			<?php endif; ?>
+		</div>
+	</div>
 <?php get_footer(); ?>
 <?php get_footer(); ?>
