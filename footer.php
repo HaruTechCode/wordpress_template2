0d8ee343252e13b6160e03dc32fc05f6f7cf3986
HaruTechCode <harutechcode@gmail.com>
2023-08-29 17:34:35 +0900

    footerのcontactの画像を修正

--- footer.php
+++ footer.php
@@ -5,11 +5,11 @@
         </div>
         <div class="footer__contact-body">
             <p class="footer__contact-text">輸入車の購入や修理のご相談など<br>お気軽にお問い合わせください。</p>
-            <a class="footer__contact-button background-flow-left">
-                <span>
-                    <i class="fa-solid fa-envelope _fa"></i>
-                    <p class="footer__contact-button-text">お問い合わせ</p>
-                </span>
+            <a class="footer__contact-button background-flow-left" href="<?php echo esc_url(home_url('contact')); ?>">
+                <svg xmlns="http://www.w3.org/2000/svg" width="22" height="17.6" viewBox="0 0 22 17.6" class="background-flow-left__img">
+                    <path id="Icon_material-mail-outline" data-name="Icon material-mail-outline" d="M22.8,6H5.2A2.2,2.2,0,0,0,3.011,8.2L3,21.4a2.206,2.206,0,0,0,2.2,2.2H22.8A2.206,2.206,0,0,0,25,21.4V8.2A2.206,2.206,0,0,0,22.8,6Zm0,15.4H5.2v-11L14,15.9l8.8-5.5ZM14,13.7,5.2,8.2H22.8Z" transform="translate(-3 -6)" fill="#fff"/>
+                </svg>
+                <p class="footer__contact-button-text background-flow-left__text">お問い合わせ</p>
             </a>
             <a href="tel:0000-000-000" class="footer__contact-button">
                 <img src="<?php echo esc_url(get_theme_file_uri("/images/phone-icon.svg")) ?>" alt="" class="">
