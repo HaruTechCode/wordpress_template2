dfdce73ba5c91589da126990fa3a6ff057e0142a
HaruTechCode <harutechcode@gmail.com>
2023-09-02 22:04:01 +0900

    404.phpのコーディング

--- 404.php
+++ 404.php
@@ -1,8 +1,20 @@
 <?php get_header(); ?>
-<main>
-    <div class="404">
-        <h2 class="404__title">404&nbsp;Not&nbsp;Found</h2>
-        <p class="404__text">
+<main class="not-found">
+    <section class="page-fv not-found-fv">
+        <div class="page-fv__inner">
+            <img src="<?php echo esc_url(get_theme_file_uri("/images/404pc.jpg")) ?>" alt="" class="page-fv__img md-none">
+            <img src="<?php echo esc_url(get_theme_file_uri("/images/404sp.jpg")) ?>" alt="" class="page-fv__img md-show">
+            <div class="not-found-fv__body">
+                <h1 class="not-found-fv__title">404 Not Found</h1>
+                <p class="not-found-fv__text md-none">
+                    お探しのページはURLが誤っているか、削除された可能性があります。<br>3秒後にTOPページに遷移します
+                </p>
+            </div>
+        </div>
+    </section>
+
+    <div class="inner">
+        <p class="not-found__text md-show">
             お探しのページはURLが誤っているか、削除された可能性があります。<br>3秒後にTOPページに遷移します
         </p>
     </div>
